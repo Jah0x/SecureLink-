bc6cd5afb6d8c32023892bde4a8fd2ec5827ecf9
Jah0x <petra12231@gmail.com>
2025-08-15 22:35:22 +0300

    refactor: sync migrations with schema

--- migrations/3/down.sql
+++
@@ -1,5 +0,0 @@
-
-ALTER TABLE vpn_users DROP COLUMN marzban_user_id;
-ALTER TABLE vpn_users DROP COLUMN marzban_username;
-ALTER TABLE vpn_subscriptions DROP COLUMN marzban_subscription_url;
-DROP TABLE marzban_config;
